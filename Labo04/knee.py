689e06cf5620b27138b2df66ede3ff42e9a14fbe
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2018-05-03 12:30:53 +0200

    quadrillage

--- Labo04/knee.py
+++ Labo04/knee.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import vtk
 
 def newRenderer(actors):
@@ -5,7 +7,7 @@ def newRenderer(actors):
     for actor in actors:
         renderer.AddActor(actor)
     renderer.SetBackground(1, 1, 1)
-    renderer.ResetCamera()
+    #renderer.ResetCamera()
     return renderer
 
 # on lit les données depuis le fichier pour créer un ensemble structuré de points (volume)
@@ -44,29 +46,61 @@ skinMapper.SetInputConnection(skinSurface.GetOutputPort())
 boneActor = vtk.vtkActor()
 boneActor.SetMapper(boneMapper)
 boneActor.GetProperty().SetPointSize(3)
+boneActor.GetProperty().SetColor(250, 0, 0)
+boneActor.GetProperty().SetAmbient(0.4)
 
 skinActor = vtk.vtkActor()
 skinActor.SetMapper(skinMapper)
 skinActor.GetProperty().SetPointSize(3)
+skinActor.GetProperty().SetColor(0.5, 0, 0)
+skinActor.GetProperty().SetAmbient(0.4)
 
-# mise en place du rendu et des acteurs
-ringRenderer = newRenderer({skinActor, boneActor})
-transparentRenderer = newRenderer({skinActor, boneActor})
-normalRenderernderer = newRenderer({skinActor, boneActor})
-proximityRenderer = newRenderer({skinActor, boneActor})
 
-# création de la fenêtre de rendu
-renderWindow = vtk.vtkRenderWindow()
-renderWindow.AddRenderer(ringRenderer)
-renderWindow.AddRenderer(transparentRenderer)
-renderWindow.AddRenderer(normalRenderernderer)
-renderWindow.AddRenderer(proximityRenderer)
+# calcul de la taille du cube en fonction des limites de la peau
+bounds = skinMapper.GetBounds()
+xLength = bounds[1] - bounds[0]
+yLength = bounds[3] - bounds[2]
+zLength = bounds[5] - bounds[4]
+xCenter = (xLength / 2) + bounds[0]
+yCenter = (yLength / 2) + bounds[2]
+zCenter = (zLength / 2) + bounds[4]
+
+# création du cube qui fait office de grillage
+grillage = vtk.vtkCubeSource()
+
+grillage.SetXLength(xLength)
+grillage.SetYLength(yLength)
+grillage.SetZLength(zLength)
+grillage.SetCenter(xCenter, yCenter, zCenter)
+
+grillageFilter = vtk.vtkOutlineFilter()
+grillageFilter.SetInputConnection(grillage.GetOutputPort())
+
+mapperGrillage = vtk.vtkPolyDataMapper()
+mapperGrillage.SetInputConnection(grillageFilter.GetOutputPort())
+
+actorGrillage = vtk.vtkActor()
+actorGrillage.SetMapper(mapperGrillage)
+actorGrillage.GetProperty().SetColor(0,0,0)
+
+# mise en place des rendus et des acteurs
+ringRenderer = newRenderer({skinActor, boneActor, actorGrillage})
+transparentRenderer = newRenderer({skinActor, boneActor, actorGrillage})
+normalRenderernderer = newRenderer({skinActor, boneActor, actorGrillage})
+proximityRenderer = newRenderer({skinActor, boneActor, actorGrillage})
 
 # découpe la fenêtre pour placer les différents rendus
 ringRenderer.SetViewport(0.0, 0.5, 0.5, 1.0)
 transparentRenderer.SetViewport(0.5, 0.5, 1.0, 1.0)
 normalRenderernderer.SetViewport(0.0, 0.0, 0.5, 0.5)
 proximityRenderer.SetViewport(0.5, 0.0, 1.0, 0.5)
+
+# création de la fenêtre d'affichage
+renderWindow = vtk.vtkRenderWindow()
+renderWindow.AddRenderer(ringRenderer)
+renderWindow.AddRenderer(transparentRenderer)
+renderWindow.AddRenderer(normalRenderernderer)
+renderWindow.AddRenderer(proximityRenderer)
 
 # paramètre l'interaction avec la fenêtre
 renderWindowInteractor = vtk.vtkRenderWindowInteractor()
