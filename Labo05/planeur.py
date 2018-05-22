c783ae342457c7343abea3b010ae5311e14afd0e
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2018-05-22 12:56:24 +0200

    file read

--- Labo05/planeur.py
+++ Labo05/planeur.py
@@ -1,1 +1,18 @@
-{\rtf1}
+import vtk
+import numpy as np
+import sys
+from dateutil import parser
+
+GLIDER_FILE_PATH = "vtkgps.txt"
+MAP_FILE_PATH = "EarthEnv-DEM90_N60E010.bil"
+
+MAP_SIZE_X = 6000
+MAP_SIZE_Y = 6000
+
+gliderCoordinates = np.genfromtxt(GLIDER_FILE_PATH, dtype=[('x', 'i4'),('y', 'i4'), ('alt', 'f4'), ('date', 'U30')], usecols=(1, 2, 3, 4, 5), skip_header=1, names=('x', 'y', 'altitude', 'date'), encoding='utf-8')
+
+mapData = np.fromfile(MAP_FILE_PATH)
+mapData.reshape(MAP_SIZE_X, MAP_SIZE_Y)
+
+print(mapData)
+print(gliderCoordinates[0])
