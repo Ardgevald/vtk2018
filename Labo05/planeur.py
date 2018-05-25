e48a8e1f1dd08b4440fdaf9747384f103c3aa84b
jimmyVerdasca <jimmy.verdasca@heig-vd.ch>
2018-05-25 10:52:28 +0200

    premier affichage

--- Labo05/planeur.py
+++ Labo05/planeur.py
@@ -6,7 +6,7 @@ import pyproj
 from math import pi, floor, ceil
 
 # distance de la caméra en proportion du rayon de la terre
-distanceFactor = 1.085
+distanceFactor = 1.01
 
 EARTH_RADIUS = 6371009
 
@@ -30,8 +30,6 @@ xBD, yBD = 1371835, 7006362
 MIN_LONG, MIN_LAT = pyproj.transform(coordinateSwedish,coordinateGlobal,min(xHG, xBG), min(yHD, yHG))
 MAX_LONG, MAX_LAT = pyproj.transform(coordinateSwedish,coordinateGlobal, max(xHD, xBD), max(yBD, yBG))
 
-print(MIN_LONG, MIN_LAT, MAX_LONG, MAX_LAT)
-
 MIN_X = floor((MIN_LONG - 10) * MAP_SIZE_X/5)
 MAX_X = ceil((MAX_LONG - 10) * MAP_SIZE_X/5)
 MIN_Y = floor((MIN_LAT - 65) * MAP_SIZE_Y/-5)
@@ -43,12 +41,14 @@ MEAN_LAT = np.mean([MIN_LAT, MAX_LAT])
 MAP_REDUCED_SIZE_X = MAX_X - MIN_X
 MAP_REDUCED_SIZE_Y = MAX_Y - MIN_Y
 
+# transforme un angle en degrés vers des radians
+def angleToRad(angle):
+    return angle * pi / 180
+
 gliderCoordinates = np.genfromtxt(GLIDER_FILE_PATH, dtype=[('x', 'i4'),('y', 'i4'), ('alt', 'f4'), ('date', 'U30')], usecols=(1, 2, 3, 4, 5), skip_header=1, names=('x', 'y', 'altitude', 'date'), encoding='utf-8')
 
 mapData = np.fromfile(MAP_FILE_PATH, dtype=np.int16)
 
-print(mapData)
-print(gliderCoordinates[0])
 mapData = mapData.reshape(MAP_SIZE_X, MAP_SIZE_Y)
 
 #création de la map
@@ -57,7 +57,7 @@ structuredGrid.SetDimensions([MAP_REDUCED_SIZE_X, MAP_REDUCED_SIZE_Y, 1])
 
 points = vtk.vtkPoints()
 points.Allocate(MAP_REDUCED_SIZE_X * MAP_REDUCED_SIZE_Y)
-'''
+
 
 #prise en compte de l'altitude
 scalars = vtk.vtkIntArray()
@@ -66,25 +66,25 @@ scalars.SetNumberOfComponents(1)
 vectorUnityLonX = (xBG - xHG) / MAP_SIZE_Y
 vectorUnityLonY = (yBD - yHD) / MAP_SIZE_Y
 
-print("vectorisation")
+for lon, y in zip(np.arange(MIN_LONG, MAX_LONG, (MAX_LONG - MIN_LONG) / MAP_REDUCED_SIZE_X), range(MIN_Y, MAX_Y)):
+    for lat, x in zip(np.arange(MIN_LAT, MAX_LAT, (MAX_LAT - MIN_LAT) / MAP_REDUCED_SIZE_Y), range(MIN_X, MAX_X)):
+        if y == MIN_Y and x == MIN_X:
+            print("Min")
+            print(y)
+            print(x)
+        if y == MAX_Y -1 and x == MAX_X -1:
+            print("max")
+            print(y)
+            print(x)
 
-for x in range(0, MAP_SIZE_X):
-    vectorUnityLatX = (xHD - xHG) * (x / MAP_SIZE_X) + (xBD - xBG) * ((MAP_SIZE_X - x) / MAP_SIZE_X)
-    vectorUnityLatY = (yHD - yHG) * (x / MAP_SIZE_X) + (yBD - yBG) * ((MAP_SIZE_X - x) / MAP_SIZE_X)
-    for y in range(0, MAP_SIZE_Y):
-
-        latSwedish = x * vectorUnityLatX + y * vectorUnityLonX
-        lonSwedish = x * vectorUnityLatY + y * vectorUnityLonY
-        latGlobal, lonGlobal = pyproj.transform(coordinateSwedish,coordinateGlobal,latSwedish,lonSwedish)
-
-        alt = mapData[y][x]
-
+        alt = EARTH_RADIUS + mapData[y][x]
         points.InsertNextPoint(
             alt,
-            angleToRad(latGlobal),
-            angleToRad(lonGlobal)
+            angleToRad(lat),
+            angleToRad(lon)
         )
-
+        scalars.InsertNextValue(mapData[y % MAP_REDUCED_SIZE_X][y // MAP_REDUCED_SIZE_Y])
+        
 structuredGrid.SetPoints(points)
 structuredGrid.GetPointData().SetScalars(scalars)
 
@@ -92,16 +92,14 @@ geometryFilter = vtk.vtkStructuredGridGeometryFilter()
 geometryFilter.SetInputData(structuredGrid)
 
 
-application d'une transformation convertissant les altitudes latitudes et longitudes
-en coordonnées sur les axes orthogonaux
+'''application d'une transformation convertissant les altitudes latitudes et longitudes
+en coordonnées sur les axes orthogonaux'''
 
 tf = vtk.vtkSphericalTransform()
 transformFilter = vtk.vtkTransformPolyDataFilter()
 transformFilter.SetTransform(tf)
 transformFilter.SetInputConnection(geometryFilter.GetOutputPort())
 transformFilter.Update()
-
-print("end sphericaltransform")
 
 # mapper sur lequel on met la lookup table pour la coloration
 mapMapper = vtk.vtkPolyDataMapper()
@@ -123,7 +121,6 @@ renWin.SetSize(800, 600)
 
 renWin.Render()
 
-print("camera position")
 # caméra posée au dessus du centre de la carte
 cameraPosIn = [distanceFactor * EARTH_RADIUS, angleToRad(MEAN_LAT), angleToRad(MEAN_LONG)]
 cameraPosOut = [0, 0, 0]
@@ -147,5 +144,3 @@ iren.SetInteractorStyle(style)
 iren.Initialize()
 iren.Start()
 
-print("started")
-'''
